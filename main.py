ee2db85109036945a441b4d4c0b83b6d9bcf2059
Ashesha1-Coder <asheshakaushik21@gmail.com>
2024-02-05 22:55:53 +0530

    update the code

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ import requests
 
 static_URL = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&fields=items/contentDetails/videoId,nextPageToken&key={}&playlistId={}&pageToken='
 
-api_key = os.environ.get('YT_API_KEY')
+api_key = 'AIzaSyAV5YZSF68n_kJi5JywH37dJZWO__vmYo4' # os.environ.get('YT_API_KEY')
+print("Api Key", api_key)
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 # To get the playlistId from the link
 def pl_id(playlist_link):
+    # Regex
     p_link = re.compile('^([\S]+list=)?([\w_-]+)[\S]*$')
     m_link = p_link.match(playlist_link)
     if m_link:
@@ -24,6 +26,7 @@ def pl_id(playlist_link):
         return 'invalid_playlist_link'
 
 
+# Patter of the Data Received from the YT API
 hours_pattern = re.compile(r'(\d+)H')
 minutes_pattern = re.compile(r'(\d+)M')
 seconds_pattern = re.compile(r'(\d+)S')
@@ -39,6 +42,7 @@ def seconds_to_time(total_seconds):
                 return f'{int(seconds)} Seconds'
             return f'{int(minutes)} Minutes, {int(seconds)} Seconds'
         return f'{int(hours)} Hours, {int(minutes)} Minutes, {int(seconds)} Seconds'
+    
     return f'{int(days)} Days, {int(hours)} Hours, {int(minutes)} Minutes, {int(seconds)} Seconds'
 
 app = Flask(__name__)
@@ -54,13 +58,19 @@ def home():
         if pl_ID == 'invalid_playlist_link':
             display_text = ["The playlist identified with the request's playlistId parameter cannot be found.", "Please retry again with correct parameters."]
             return render_template("home.html", display_text = display_text)
-
+        
     vid_counter = 0
     total_seconds = 0
     nextPageToken = None
     next_page = ''   
     chart_data = [ [], [] ]
+    # Palyist REquest -> Vid ID
+    # for each Vid ID
+    #       ConteentDetials -> duration
+    #       snippet -> Title
     while True:
+
+        # Checking if the palylist is valid or not
         try:
             temp_req = json.loads(requests.get(static_URL.format(api_key, pl_ID) + next_page).text)
             if "error" in temp_req:
@@ -68,18 +78,22 @@ def home():
         except KeyError:
             display_text = ["The playlist identified with the request's playlistId parameter cannot be found.", "Please retry again with correct parameters."]
             return render_template("home.html", display_text = display_text)
+        
+        # If it is valid, creat the request
         pl_request = youtube.playlistItems().list(
                 part='contentDetails',
                 playlistId=pl_ID,
                 maxResults=50,
                 pageToken=nextPageToken
             )
-
+        # Execute the request and get the response
         pl_response = pl_request.execute()
         vid_ids = []
+
         for item in pl_response['items']:
             vid_ids.append(item['contentDetails']['videoId'])
-
+        
+        # This is the reuest to gee details for the video. Title, Time
         vid_request = youtube.videos().list(
                             part="contentDetails",
                             id=','.join(vid_ids)
@@ -113,16 +127,20 @@ def home():
                 minutes=minutes,
                 seconds=seconds
             ).total_seconds()
-
+            
+            # Total Secods of the paylis
             total_seconds += video_seconds
             chart_data[0].append(video_title)
             chart_data[1].append( video_seconds / 60)
 
+        # This is required for the next rrequest, 50 -50 videos acn be fetched in one requeset from playlist
         nextPageToken = pl_response.get('nextPageToken')
+
         vid_counter = len(chart_data[1])
         if not nextPageToken:
             break
-        
+    
+    #### Now we have all the data ####
     display_text = []
     display_text += ['No of videos : ' + str(vid_counter),
         'Average length of a video : ' + seconds_to_time(total_seconds/vid_counter), 
@@ -137,3 +155,26 @@ def home():
 
 if __name__ == "__main__":
     app.run(use_reloader=True, debug=False)
+
+
+
+
+
+
+# Sure, I'd be happy to explain this Python regular expression to you!
+
+# This regular expression `^([\S]+list=)?([\w_-]+)[\S]*$` can be broken down as follows:
+
+# - `^` : This asserts the start of a line.
+# - `([\S]+list=)?` : This is a group that matches one or more non-whitespace characters (`[\S]+`) followed by the
+#  string 'list='. The question mark at the end makes this group optional, meaning it will match lines with or without this pattern.
+# - `([\w_-]+)` : This is another group that matches one or more word characters (which include alphanumeric 
+# characters and underscores) or hyphens.
+# - `[\S]*` : This matches zero or more non-whitespace characters.
+# - `$` : This asserts the end of a line.
+
+# So, in summary, this regular expression will match any line that optionally 
+# starts with one or more non-whitespace characters followed by 'list=', then 
+# has one or more word characters or hyphens, and ends with zero or more non-whitespace 
+# characters. The two groups in parentheses are capturing groups, which means the portions 
+# of the input they match will be saved for later use.
