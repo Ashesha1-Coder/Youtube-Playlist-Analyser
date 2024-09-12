8903097ea3299a376a0a656a2ab7900d6082dc49
Ashesha1-Coder <asheshakaushik21@gmail.com>
2024-09-12 17:09:39 +0530

    modified the code using solid principles

--- src/main.py
+++ src/main.py
@@ -4,169 +4,179 @@ import re
 from datetime import timedelta
 from googleapiclient.discovery import build
 from flask import Flask, Response, request, render_template, url_for
-import re
 import requests
 
+class YouTubeAPI:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.youtube = build('youtube', 'v3', developerKey=api_key)
+        self.static_url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&fields=items/contentDetails/videoId,nextPageToken&key={}&playlistId={}&pageToken='
 
-static_URL = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&fields=items/contentDetails/videoId,nextPageToken&key={}&playlistId={}&pageToken='
-
-api_key = 'AIzaSyCYIU3i74rWF1ex3ebLUXMRbIkJArL0EOE'
-
-
-
-
-
-
-
-# os.environ.get('YT_API_KEY')
-print("Api Key", api_key)
-
-youtube = build('youtube', 'v3', developerKey = api_key)
-
-# To get the playlistId from the link
-def pl_id(playlist_link):
-    # Regex
-    p_link = re.compile(r'^([\S]+list=)?([\w_-]+)[\S]*$')
-    m_link = p_link.match(playlist_link)
-    if m_link:
-        return m_link.group(2)
-    else:
+    def get_playlist_id(self, playlist_link):
+        pattern = re.compile(r'^([\S]+list=)?([\w_-]+)[\S]*$')
+        match = pattern.match(playlist_link)
+        if match:
+            return match.group(2)
         return 'invalid_playlist_link'
 
+    def fetch_playlist_items(self, playlist_id, page_token=''):
+        url = self.static_url.format(self.api_key, playlist_id) + page_token
+        response = requests.get(url)
+        return response.json()
 
-# Patter of the Data Received from the YT API
-hours_pattern = re.compile(r'(\d+)H')
-minutes_pattern = re.compile(r'(\d+)M')
-seconds_pattern = re.compile(r'(\d+)S')
+class TimeFormatter:
+    hours_pattern = re.compile(r'(\d+)H')
+    minutes_pattern = re.compile(r'(\d+)M')
+    seconds_pattern = re.compile(r'(\d+)S')
 
-# To seconds_to_time the datetime object into readable time
-def seconds_to_time(total_seconds):
-    minutes, seconds = divmod(total_seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours =  divmod(hours, 24)
-    if days == 0:
-        if hours == 0:
-            if minutes == 0:
-                return f'{int(seconds)} Seconds'
-            return f'{int(minutes)} Minutes, {int(seconds)} Seconds'
-        return f'{int(hours)} Hours, {int(minutes)} Minutes, {int(seconds)} Seconds'
+    @staticmethod
+    def seconds_to_time(total_seconds):
+        minutes, seconds = divmod(total_seconds, 60)
+        hours, minutes = divmod(minutes, 60)
+        days, hours = divmod(hours, 24)
+        if days == 0:
+            if hours == 0:
+                if minutes == 0:
+                    return f'{int(seconds)} Seconds'
+                return f'{int(minutes)} Minutes, {int(seconds)} Seconds'
+            return f'{int(hours)} Hours, {int(minutes)} Minutes, {int(seconds)} Seconds'
+        return f'{int(days)} Days, {int(hours)} Hours, {int(minutes)} Minutes, {int(seconds)} Seconds'
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/playlist', methods=['POST'])
+def get_playlist():
+    playlist_link = request.form['playlist_link']
+    yt_api = YouTubeAPI(api_key)
+    playlist_id = yt_api.get_playlist_id(playlist_link)
+    if playlist_id == 'invalid_playlist_link':
+        return Response('Invalid playlist link', status=400)
     
-    return f'{int(days)} Days, {int(hours)} Hours, {int(minutes)} Minutes, {int(seconds)} Seconds'
+    playlist_items = yt_api.fetch_playlist_items(playlist_id)
+    return Response(json.dumps(playlist_items), mimetype='application/json')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+import json
+import re
+from datetime import timedelta
+from googleapiclient.discovery import build
+from flask import Flask, Response, request, render_template
+import requests
+
+class YouTubeAPI:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.youtube = build('youtube', 'v3', developerKey=api_key)
+        self.static_url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&fields=items/contentDetails/videoId,nextPageToken&key={}&playlistId={}&pageToken='
+
+    def get_playlist_id(self, playlist_link):
+        pattern = re.compile(r'^([\S]+list=)?([\w_-]+)[\S]*$')
+        match = pattern.match(playlist_link)
+        if match:
+            return match.group(2)
+        return 'invalid_playlist_link'
+
+    def fetch_playlist_items(self, playlist_id, page_token=''):
+        url = self.static_url.format(self.api_key, playlist_id) + page_token
+        response = requests.get(url)
+        return response.json()
+
+    def fetch_video_details(self, video_ids):
+        vid_request = self.youtube.videos().list(part="contentDetails", id=','.join(video_ids))
+        title_request = self.youtube.videos().list(part="snippet", id=','.join(video_ids))
+        vid_response = vid_request.execute()
+        title_response = title_request.execute()
+        return vid_response, title_response
+
+class TimeFormatter:
+    hours_pattern = re.compile(r'(\d+)H')
+    minutes_pattern = re.compile(r'(\d+)M')
+    seconds_pattern = re.compile(r'(\d+)S')
+
+    @staticmethod
+    def seconds_to_time(total_seconds):
+        minutes, seconds = divmod(total_seconds, 60)
+        hours, minutes = divmod(minutes, 60)
+        days, hours = divmod(hours, 24)
+        if days == 0:
+            if hours == 0:
+                if minutes == 0:
+                    return f'{int(seconds)} Seconds'
+                return f'{int(minutes)} Minutes, {int(seconds)} Seconds'
+            return f'{int(hours)} Hours, {int(minutes)} Minutes, {int(seconds)} Seconds'
+        return f'{int(days)} Days, {int(hours)} Hours, {int(minutes)} Minutes, {int(seconds)} Seconds'
+
+class PlaylistAnalyzer:
+    def __init__(self, api_key):
+        self.youtube_api = YouTubeAPI(api_key)
+        self.time_formatter = TimeFormatter()
+
+    def analyze_playlist(self, playlist_link):
+        playlist_id = self.youtube_api.get_playlist_id(playlist_link)
+        if playlist_id == 'invalid_playlist_link':
+            return ["The playlist identified with the request's playlistId parameter cannot be found.", "Please retry again with correct parameters."]
+
+        vid_counter = 0
+        total_seconds = 0
+        nextPageToken = None
+        chart_data = [[], []]
+
+        while True:
+            pl_response = self.youtube_api.fetch_playlist_items(playlist_id, nextPageToken)
+            vid_ids = [item['contentDetails']['videoId'] for item in pl_response['items']]
+            vid_response, title_response = self.youtube_api.fetch_video_details(vid_ids)
+
+            for item_time, item_title in zip(vid_response['items'], title_response['items']):
+                duration = item_time['contentDetails']['duration']
+                video_title = item_title['snippet']['title']
+
+                hours = self.time_formatter.hours_pattern.search(duration)
+                minutes = self.time_formatter.minutes_pattern.search(duration)
+                seconds = self.time_formatter.seconds_pattern.search(duration)
+
+                hours = int(hours.group(1)) if hours else 0
+                minutes = int(minutes.group(1)) if minutes else 0
+                seconds = int(seconds.group(1)) if seconds else 0
+
+                video_seconds = timedelta(hours=hours, minutes=minutes, seconds=seconds).total_seconds()
+                total_seconds += video_seconds
+                chart_data[0].append(video_title)
+                chart_data[1].append(video_seconds / 60)
+
+            nextPageToken = pl_response.get('nextPageToken')
+            vid_counter = len(chart_data[1])
+            if not nextPageToken:
+                break
+
+        display_text = [
+            'No of videos : ' + str(vid_counter),
+            'Average length of a video : ' + self.time_formatter.seconds_to_time(total_seconds / vid_counter),
+            'Total length of playlist : ' + self.time_formatter.seconds_to_time(total_seconds),
+            'At 1.25x : ' + self.time_formatter.seconds_to_time(total_seconds / 1.25),
+            'At 1.50x : ' + self.time_formatter.seconds_to_time(total_seconds / 1.5)
+        ]
+        return display_text
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    if(request.method == 'GET'):
+    if request.method == 'GET':
         return render_template("home.html")
-    else :
-        # get playlist link/id as input from the form 
+    else:
         user_link = request.form.get('search_string').strip()
-        pl_ID = pl_id(user_link)
-        if pl_ID == 'invalid_playlist_link':
-            display_text = ["The playlist identified with the request's playlistId parameter cannot be found.", "Please retry again with correct parameters."]
-            return render_template("home.html", display_text = display_text)
-        
-    vid_counter = 0
-    total_seconds = 0
-    nextPageToken = None
-    next_page = ''   
-    chart_data = [ [], [] ]
-    # Playist REquest -> Vid ID
-    # for each Vid ID
-    #       ContentDetials -> duration
-    #       snippet -> Title
-    while True:
+        analyzer = PlaylistAnalyzer(api_key)
+        display_text = analyzer.analyze_playlist(user_link)
+        return render_template("home.html", display_text=display_text)
 
-        # Checking if the palylist is valid or not
-        try:
-            temp_req = json.loads(requests.get(static_URL.format(api_key, pl_ID) + next_page).text)
-            if "error" in temp_req:
-                raise KeyError
-        except KeyError:
-            display_text = ["The playlist identified with the request's playlistId parameter cannot be found.", "Please retry again with correct parameters."]
-            return render_template("home.html", display_text = display_text)
-        
-        # If it is valid, creat the request
-        pl_request = youtube.playlistItems().list(
-                part='contentDetails',
-                playlistId=pl_ID,
-                maxResults=50,
-                pageToken=nextPageToken
-            )
-        # Execute the request and get the response
-        pl_response = pl_request.execute()
-        vid_ids = []
-
-        for item in pl_response['items']:
-            vid_ids.append(item['contentDetails']['videoId'])
-        
-        # This is the reuest to gee details for the video. Title, Time
-        vid_request = youtube.videos().list(
-                            part="contentDetails",
-                            id=','.join(vid_ids)
-                        )
-
-        title_request = youtube.videos().list(
-                        part="snippet",
-                        id=','.join(vid_ids)
-                    )
-
-
-        vid_response = vid_request.execute()
-        title_response = title_request.execute()
-
-        for item_time, item_title in zip(vid_response['items'], title_response['items']):
-            duration = item_time['contentDetails']['duration']
-            
-            video_title = item_title['snippet']['title']
-            # print(f"Titile is :- {video_title}")
-            
-            hours = hours_pattern.search(duration)
-            minutes = minutes_pattern.search(duration)
-            seconds = seconds_pattern.search(duration)
-
-            hours = int(hours.group(1)) if hours else 0
-            minutes = int(minutes.group(1)) if minutes else 0
-            seconds = int(seconds.group(1)) if seconds else 0
-
-            video_seconds = timedelta(
-                hours=hours,
-                minutes=minutes,
-                seconds=seconds
-            ).total_seconds()
-            
-            # Total Secods of the paylis
-            total_seconds += video_seconds
-            chart_data[0].append(video_title)
-            chart_data[1].append( video_seconds / 60)
-
-        # This is required for the next rrequest, 50 -50 videos acn be fetched in one requeset from playlist
-        nextPageToken = pl_response.get('nextPageToken')
-
-        vid_counter = len(chart_data[1])
-        if not nextPageToken:
-            break
-    
-    #### Now we have all the data ####
-    display_text = []
-    display_text += ['No of videos : ' + str(vid_counter),
-        'Average length of a video : ' + seconds_to_time(total_seconds/vid_counter), 
-        'Total length of playlist : ' + seconds_to_time(total_seconds), 
-        'At 1.25x : ' + seconds_to_time(total_seconds/1.25), 
-        'At 1.50x : ' + seconds_to_time(total_seconds/1.5), 
-        'At 1.75x : ' + seconds_to_time(total_seconds/1.75), 
-        'At 2.00x : ' + seconds_to_time(total_seconds/2)]
-
-    return render_template("home.html", display_text = display_text, chart_data = chart_data)
-
-
-if __name__ == "__main__":
-    app.run(use_reloader=True, debug=False)
-
-
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 # Sure, I'd be happy to explain this Python regular expression to you!
